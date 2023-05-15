d21a7137c93d646c9a0936eb22fa9c2d6abf554d
Naser Ahadi <naserahadi95@gmail.com>
2023-05-15 21:35:38 +0330

    add enum

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 // explicitly setting type of roles
-var person = {
-    name: 'Naser',
-    age: 32,
-    hobbies: ['chess', 'watch movie'],
-    role: [2, 'author']
-};
-// const person = {
+// const person:{
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];
+// } = {
 //     name: 'Naser',
 //     age: 32,
 //     hobbies: ['chess', 'watch movie'],
 //     role: [2, 'author']
 // }
-person.role.push('admin');
-// person.role[1]=10 // get error
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "admin";
+    Role[Role["READ_ONLY"] = 0] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 1] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Naser',
+    age: 32,
+    hobbies: ['chess', 'watch movie'],
+    role: Role.ADMIN
+};
+// push method is an exception which even by using Tuple, Typescript can't catch error 
+// person.role.push('admin')
+// person.role[1]=10 // because of using Tuple, catch error
 var favoriteActivities;
 favoriteActivities = ['sport', 'movie'];
 console.log(person.name);
@@ -20,4 +32,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
     // console.log(hobby.map()); // !!! ERROR !!!
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
