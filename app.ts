0e31643ac650b8dfd61744853fb8fb6ba9b2f3c0
Naser Ahadi <naserahadi95@gmail.com>
2023-05-30 14:11:52 +0330

    unknown type

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
-var userInput;
-var userName;
+let userInput: unknown;
+let userName: string;
+
 userInput = 5;
 userName = "Max";
 // error: Type 'unknown' is not assignable to type 'string'.
 // userName = userInput;
+
 // extra assertion to assign unknown into string
 if (typeof userInput === "string") {
-    userName = userInput;
+  userName = userInput;
 }
