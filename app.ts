72bb5398d9ade4b820116196649a65200404ae32
Naser Ahadi <naserahadi95@gmail.com>
2023-05-30 11:33:08 +0330

    void in typescript

--- app.ts
+++ app.ts
@@ -1,28 +1,11 @@
-// UNION TYPES
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+// function return number
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combineAges = combine(30, 56, "as-number");
-console.log(combineAges);
+// void => function return nothing so its type is void
+function printResult(num: number) {
+  console.log("result: " + num);
+}
 
-const combineStringAges = combine("30", "56", "as-number");
-console.log(combineStringAges);
-
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+printResult(add(5, 3));
