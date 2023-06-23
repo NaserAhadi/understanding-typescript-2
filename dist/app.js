323a8412048004d4f8817c4ea178d7b7c6219fa4
Naser Ahadi <n.ahadi@vandar.io>
2023-06-23 17:18:39 +0330

    use removeComments property

--- dist/app.js
+++ dist/app.js
@@ -3,19 +3,12 @@ let userInput;
 let userName;
 userInput = 5;
 userName = "Max";
-// error: Type 'unknown' is not assignable to type 'string'.
-// userName = userInput;
-// extra assertion to assign unknown into string
 if (typeof userInput === "string") {
     userName = userInput;
 }
 function generateError(message, code) {
     throw { message: message, codeError: code };
 }
-// generateError("An error occured", 500);
-// if log the function we see it never return anything
-// const result = generateError("An error occured", 500);
-// console.log(result);
 console.log(555);
 const button = document.querySelector("button");
 button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
